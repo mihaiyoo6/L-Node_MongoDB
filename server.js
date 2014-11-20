50d80393e4c28ddca0760744fb97e2f1d7597635
MihaiMijea <mijeamihai@yahoo.com>
2014-11-20 06:56:58 +0100

    sublime text test

--- server.js
+++ server.js
@@ -9,17 +9,3 @@ app = config(app);
 var server = app.listen(app.get('port'), function() {
     console.log('Server up: http://localhost:' + app.get('port'));
 });
-//test git sublime
-/*p = config(app);
-
-var server = app.listen(app.get('port'), function() {
-    console.log('Server up: http://localhost:' + app.get('port'));
-})p = config(app);
-
-var server = app.listen(app.get('port'), function() {
-    console.log('Server up: http://localhost:' + app.get('port'));
-})p = config(app);
-
-var server = app.listen(app.get('port'), function() {
-    console.log('Server up: http://localhost:' + app.get('port'));
-})]*/
