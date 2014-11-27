deed56361d568ac3bd81ceb47b57a7843557f8d6
MihaiMijea <mijeamihai@yahoo.com>
2014-11-27 06:47:33 +0100

    Add connection to MongoDB, create home and image models, update home and image controller (working add image and update vies)

--- controllers/home.js
+++ controllers/home.js
@@ -1,46 +1,20 @@
-var sidebar = require('../helpers/sidebar');
+var sidebar = require('../helpers/sidebar'),
+	ImageModel = require('../models').Image;
 
 module.exports = {
 	index: function (req, res){
 		var viewModel={
-			images:[
-				{
-					uniqueId:		1,
-					title:			'Sample Image 1',
-					description:	'',
-					filename:		'1.png',
-					views:			0,
-					likes:			0,
-					timestamp:		Date.now
-				},{
-					uniqueId:		2,
-					title:			'Sample Image 2',
-					description:	'',
-					filename:		'2.png',
-					views:			0,
-					likes:			0,
-					timestamp:		Date.now
-				},{
-					uniqueId:		3,
-					title:			'Sample Image 3',
-					description:	'',
-					filename:		'3.png',
-					views:			0,
-					likes:			0,
-					timestamp:		Date.now
-				},{
-					uniqueId:		4,
-					title:			'Sample Image 4',
-					description:	'',
-					filename:		'4.png',
-					views:			0,
-					likes:			0,
-					timestamp:		Date.now
-				}
-			]
+			images:{}
 		};
-		sidebar(viewModel, function(viewModel){
-			res.render('index', viewModel);
-		});
+
+		ImageModel.find({}, {}, {sort: {timestramp: -1}},
+			function(err, images){
+				if(err){ throw err;}
+
+				viewModel.images = images;
+				sidebar(viewModel, function(viewModel){
+					res.render('index', viewModel);
+				});
+			});
 	}
 };
--- controllers/image.js
+++ controllers/image.js
@@ -1,38 +1,39 @@
 var fs = require('fs'),
     path = require('path'),
     sidebar = require('../helpers/sidebar');
+    Models = require('../models');
 
 module.exports = {
     index: function(req, res) {
         var viewModel={
-            image:{
-                uniqueId: 1,
-                title: 'Sample image 1',
-                description: 'This is a sample',
-                filename: '1.png',
-                views: 0,
-                likes: 0,
-                timestamp: Date.now
-            },
-            comments:[
-                {
-                    image_id: 1,
-                    email: 'test@tesing.com',
-                    name:'Test Tester',
-                    gravatar: 'http://lorempixel.com/75/75/animals/1',
-                    comment: 'This is a test comment...'
-                },{
-                    image_id: 1,
-                    email: 'test@tesing.com',
-                    name:'Test Tester',
-                    gravatar: 'http://lorempixel.com/75/75/animals/2',
-                    comment: 'This is a test 2 comment...'
-                }
-            ]
+            image:{},
+            comments:[]
         };
-        sidebar(viewModel, function(){
-            res.render('image', viewModel);
-        });
+        Models.Image.findOne({filename: {$regex: req.params.image_id} },
+            function(err, image){
+                if(err){throw err;}
+                if(image){
+                    image.views = image.views + 1;
+                    viewModel.image = image;
+                    image.save();
+
+                    Models.Comment.find(
+                        { image_id: image._id},
+                        {},
+                        { sort: { 'timestamp': 1}},
+                        function(err, comments){
+                            if(err){throw err;}
+                            viewModel.comments = comments;
+                            sidebar(viewModel, function(){
+                                res.render('image', viewModel);
+                            });
+                        }
+                    );
+                }else{
+                    res.redirect('/');
+                }
+            });
+        
     },
     create: function(req, res) {
         var saveImage = function(){
@@ -41,21 +42,37 @@ module.exports = {
             for(var i = 0; i < 6; i+=1){
                 imgUrl += possible.charAt(Math.floor(Math.random() * possible.length));
             }
-            var tempPath = req.files.file.path,
-                ext = path.extname(req.files.file.name).toLowerCase(),
-                targetPath = path.resolve('./public/upload/' + imgUrl + ext);
 
-            if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
-                fs.rename(tempPath, targetPath, function (err){
-                    if(err) throw err;
-                    res.redirect('/images/' + imgUrl);
-                });
-            }else{
-              fs.unlink(tempPath,function  (err) {
-                if(err) throw err;
-                res.json(500, {error: 'Only image files are allowed.'});
-              });
-            }
+            Models.Image.find({ filename: imgUrl}, function(err, images){
+                if(images.length > 0){
+                    savaImage();
+                }else{
+                    var tempPath = req.files.file.path,
+                        ext = path.extname(req.files.file.name).toLowerCase(),
+                        targetPath = path.resolve('./public/upload/' + imgUrl + ext);
+
+                    if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
+                        fs.rename(tempPath, targetPath, function (err){
+                            if(err) throw err;
+                            //res.redirect('/images/' + imgUrl);
+                            var newImg= new Models.Image({
+                                title: req.body.title,
+                                description: req.body.description,
+                                filename: imgUrl + ext
+                            });
+                            newImg.save(function(err, image){
+                                console.log("successfully interted image: "+ image.filename);
+                                res.redirect('/images/'+ image.uniqueId);
+                            });
+                        });
+                    }else{
+                      fs.unlink(tempPath,function  (err) {
+                        if(err) throw err;
+                        res.json(500, {error: 'Only image files are allowed.'});
+                      });
+                    }
+                }
+            });
         };
 
         saveImage();
